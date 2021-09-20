1c959cb99ed3bfff50988cfd9aade3f6e4a8177c
nikolai.burov@stanga.net <nikolai.burov@stanga.net>
2021-09-20 13:43:24 +0300

    fixing status codes and replacing return values

--- app/Http/Constants/StatusCodes.php
+++ app/Http/Constants/StatusCodes.php
@@ -4,20 +4,22 @@ namespace App\Http\Constants;
 
 class StatusCodes
 {
-    public function postRequests(){
-            return (object)[
-                '200' => [
-                    'OK' => 'OK',
-                    'non_existent_product' => 'There is no product with such id',
-                    'product_list_empty' => 'There are no products with such ids',
-                    'empty_categories' => 'Categories list is empty',
-                    'wrong_data_type' => 'Provided value is not of type array'
-                ],
-                '404' => 'NotFound',
-                '400' => 'Bad request',
-                '406' => [
-                    'incorrect_Data' => 'Data not provided'
-                ]
-            ];
+    public function postRequests()
+    {
+        return (object)[
+            '200' => [
+                'OK' => 'OK',
+                'non_existent_product' => 'There is no product with such id',
+                'product_list_empty' => 'There are no products with such ids',
+                'empty_categories' => 'Categories list is empty',
+                'empty_flavours' => 'Flavours list is empty',
+                'wrong_data_type' => 'Provided value is not of type array'
+            ],
+            '404' => 'NotFound',
+            '400' => 'Bad request',
+            '406' => [
+                'incorrect_Data' => 'Data not provided'
+            ]
+        ];
     }
 }
